1f6b2ff5dcbeee593d30530daf97c340e88e65df
Viktor Zherekhin <zherekhinv@gmail.com>
2023-01-17 21:57:30 +0300

    Добавлен комментарий к методу getAge()

--- src/Input.java
+++ src/Input.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        String stroka = new String("novaya stroka");    // альтернативный способ задания объекта класса "String"
+        String stroka = new String ("novaya stroka");    // альтернативный способ задания объекта класса "String"
 
         /**
          * класс "Scanner" отвечает ввод данных с консоли
