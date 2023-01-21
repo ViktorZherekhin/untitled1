11f39d076ac142fd6fab6c50ea457ed45d8f0ea7
Viktor Zherekhin <zherekhinv@gmail.com>
2023-01-21 12:46:03 +0300

    Изучение модификаторов доступа (public, private, protected и default)

--- src/Forest/Tree.java
+++ src/Forest/Tree.java
@@ -1,4 +1,11 @@
 package Forest;
 
 public class Tree {
+    public static void main(String[] args) {
+        Belka belka1 = new Belka();
+        System.out.println(belka1.b2);      // получили доступ к этой переменной благодаря молификатору
+                                            // "protected" в классе, расположенном в том же пакете
+    }
+
+
 }
